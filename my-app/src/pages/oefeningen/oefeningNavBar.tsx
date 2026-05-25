22488dfb2fd7141fddb9334a72e67bf36708688f
NinaBreedstraet <nina.breedstraet@student.ehb.be>
2026-05-25 17:38:33 +0200

    Navigatiebar oefeningenpagina verbeteren

--- my-app/src/pages/oefeningen/oefeningNavBar.tsx
+++ my-app/src/pages/oefeningen/oefeningNavBar.tsx
@@ -1,3 +1,4 @@
+import { BracketItem } from "../../components/bracketItem/bracketItem";
 import style from "./oefeningNavBar.module.scss";
 
 type OefeningNavBarProps = {
@@ -16,55 +17,55 @@ export default function OefeningNavBar({
           onClick={() => setActiveTab("basis")}
           className={activeTab === "basis" ? style.active : style.link}
         >
-          Basis
+          <BracketItem>Basis</BracketItem>
         </button>
         <button
           onClick={() => setActiveTab("oefening 1")}
           className={activeTab === "oefening 1" ? style.active : style.link}
         >
-          Oefening 1
+          <BracketItem>Oefening 1</BracketItem>
         </button>
         <button
           onClick={() => setActiveTab("oefening 2")}
           className={activeTab === "oefening 2" ? style.active : style.link}
         >
-          Oefening 2
+          <BracketItem>Oefening 2</BracketItem>
         </button>
         <button
           onClick={() => setActiveTab("oefening 3")}
           className={activeTab === "oefening 3" ? style.active : style.link}
         >
-          Oefening 3
+          <BracketItem>Oefening 3</BracketItem>
         </button>
         <button
           onClick={() => setActiveTab("oefening 4")}
           className={activeTab === "oefening 4" ? style.active : style.link}
         >
-          Oefening 4
+          <BracketItem>Oefening 4</BracketItem>
         </button>
         <button
           onClick={() => setActiveTab("oefening 5")}
           className={activeTab === "oefening 5" ? style.active : style.link}
         >
-          Oefening 5
+          <BracketItem>Oefening 5</BracketItem>
         </button>
         <button
           onClick={() => setActiveTab("oefening 6")}
           className={activeTab === "oefening 6" ? style.active : style.link}
         >
-          Oefening 6
+          <BracketItem>Oefening 6</BracketItem>
         </button>
         <button
           onClick={() => setActiveTab("oefening 7")}
           className={activeTab === "oefening 7" ? style.active : style.link}
         >
-          Oefening 7
+          <BracketItem>Oefening 7</BracketItem>
         </button>
         <button
           onClick={() => setActiveTab("oefening 8")}
           className={activeTab === "oefening 8" ? style.active : style.link}
         >
-          Oefening 8
+          <BracketItem>Oefening 8</BracketItem>
         </button>
       </nav>
     </div>
