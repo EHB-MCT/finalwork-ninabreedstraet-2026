f447bf39ec0f121dc9e7b1d840599093803f1555
NinaBreedstraet <nina.breedstraet@student.ehb.be>
2026-05-26 21:43:13 +0200

    Curly brackets toegevoegd

--- my-app/src/components/bracketItem/bracketItem.tsx
+++ my-app/src/components/bracketItem/bracketItem.tsx
@@ -1,19 +1,23 @@
 import style from "./bracketItem.module.scss";
 
+type BracketType = "square" | "curly";
+
 interface BracketItemProps {
   children: React.ReactNode;
   onClick?: () => void;
   className?: string;
+  bracketType?: BracketType;
 }
 
 export function BracketItem({
   children,
   onClick,
   className = "",
+  bracketType = "square",
 }: BracketItemProps) {
   return (
     <div
-      className={`${style["bracket-item"]} ${style["bracket-item--square"]} ${className}`}
+      className={`${style["bracket-item"]} ${style[`bracket-item--${bracketType}`]}`}
       onClick={onClick}
     >
       {children}
